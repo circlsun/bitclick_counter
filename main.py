c6976c4689690e63f313afce58e05b18439800d8
circlsun <circlsun@gmail.com>
2022-12-06 10:49:35 +0300

    Move const TOKEN to main() as token

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ import os
 from dotenv import load_dotenv
 
 
-TOKEN = os.getenv('BITLY_TOKEN')
-
-
 def shorten_link(token, url):
-    headers = {'Authorization': f"Bearer {TOKEN}"}
+    headers = {'Authorization': f"Bearer {token}"}
     url_shorten = "https://api-ssl.bitly.com/v4/shorten"
     payload = {
         "long_url": url
@@ -18,7 +15,7 @@ def shorten_link(token, url):
 
 
 def count_clicks(token, bitlink):
-    headers = {"Authorization": f"Bearer {TOKEN}"}
+    headers = {"Authorization": f"Bearer {token}"}
     params = (
         ("units", "-1"),
     )
@@ -27,8 +24,8 @@ def count_clicks(token, bitlink):
     return response.json()["total_clicks"]
 
 
-def is_bitlink(url):
-    headers = {'Authorization': f"Bearer {TOKEN}"}
+def is_bitlink(token, url):
+    headers = {'Authorization': f"Bearer {token}"}
     url = f"https://api-ssl.bitly.com/v4/bitlinks/{url}"
     try:
         response = requests.get(url, headers=headers)
@@ -39,18 +36,19 @@ def is_bitlink(url):
 
 
 def main():
+    token = os.getenv('BITLY_TOKEN')
     load_dotenv()
     url = input("Введите ссылку: ")
-    if not is_bitlink(url):
+    if not is_bitlink(token, url):
         try:
-            bitlink = shorten_link(TOKEN, url)
+            bitlink = shorten_link(token, url)
         except requests.exceptions.HTTPError:
             print("Введите корректную ссылку")
         else:
             print('Битлинк:', bitlink)
     else:
         try:
-            print("Число кликов по ссылке:", count_clicks(TOKEN, url))
+            print("Число кликов по ссылке:", count_clicks(token, url))
         except requests.exceptions.HTTPError:
             print("Введите корректную ссылку")
 
